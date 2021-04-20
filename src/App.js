35450a2c6caaf13c4f358f35a0fa72fc52510dd7
Uebriges <uebriges@gmx.at>
2021-04-20 19:29:05 +0200

    Add logging of a subset of points. Start drawing austrian border.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import esriConfig from '@arcgis/core/config.js';
 import Graphic from '@arcgis/core/Graphic';
-import FeatureLayer from '@arcgis/core/layers/FeatureLayer';
 import GraphicsLayer from '@arcgis/core/layers/GraphicsLayer';
 import ArcGISMap from '@arcgis/core/Map';
-import DictionaryRenderer from '@arcgis/core/renderers/DictionaryRenderer';
 import MapView from '@arcgis/core/views/MapView';
 import { useEffect, useRef, useState } from 'react';
 import { readRemoteFile } from 'react-papaparse';
 import './App.css';
 
 // Generates the points on the map
-function pointsGenerator(points) {
-  console.log('points: ', points);
+function generateGraphicsLayerWithPoints(points) {
   const graphicsLayer = new GraphicsLayer();
   const simpleMarkerSymbol = {
     type: 'simple-marker',
@@ -48,16 +45,58 @@ function pointsGenerator(points) {
   return graphicsLayer;
 }
 
-async function logLocationData(graphicsLayer) {
-  // await graphicsLayer.load().then((data) => console.log('data: ', data));
-  // const layer = await graphicsLayer.load();
-  // console.log('layer in log: ', layer.graphics.items[0].geometry.longitude);
-}
-
 // React function component
 function App() {
   const [csvData, setCSVData] = useState();
+  const [borderPoints, setBorderPoints] = useState([]);
 
+  // Set ESRI configurations
+  esriConfig.assetsPath = './assets';
+  esriConfig.apiKey = process.env.ESRI_API_KEY;
+
+  // Ref of map
+  const mapDiv = useRef(null);
+
+  function logLocationData(csvData) {
+    if (csvData) {
+      const austrianBorderPoints = csvData.filter((point) => point[0] === 'AT');
+      console.log('Austrian border points: ', austrianBorderPoints);
+
+      // Remove country code and set state
+      const borderPointsWithoutCountryCode = austrianBorderPoints.map(
+        (borderPoint) => {
+          return [borderPoint[1], borderPoint[2]];
+        },
+      );
+      setBorderPoints(borderPointsWithoutCountryCode);
+    }
+  }
+
+  function createBorder() {
+    if (borderPoints) {
+      console.log('border points: ', borderPoints);
+
+      const polyline = {
+        type: 'polyline',
+        paths: borderPoints,
+      };
+
+      var polylineSymbol = {
+        type: 'simple-line',
+        color: [100, 80, 40],
+        width: 3,
+      };
+
+      var polylineGraphic = new Graphic({
+        geometry: polyline,
+        symbol: polylineSymbol,
+      });
+
+      return polylineGraphic;
+    }
+  }
+
+  // Read the CSV file with points data
   useEffect(() => {
     readRemoteFile(
       'https://austria.maps.arcgis.com/sharing/rest/content/items/81b6dd9c931e45ec82022a2b9cd18ca0/data',
@@ -76,11 +115,6 @@ function App() {
     );
   }, []);
 
-  esriConfig.assetsPath = './assets';
-  esriConfig.apiKey = process.env.ESRI_API_KEY;
-
-  const mapDiv = useRef(null);
-
   useEffect(() => {
     if (mapDiv.current) {
       /**
@@ -97,65 +131,19 @@ function App() {
         zoom: 3,
       });
 
-      const scale = 36112;
-      const layer1 = new FeatureLayer({
-        url:
-          'https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/Alternative_Fuel_Station_March2018/FeatureServer',
-        outFields: ['*'],
-        renderer: new DictionaryRenderer({
-          url:
-            'https://jsapi.maps.arcgis.com/sharing/rest/content/items/30cfbf36efd64ccf92136201d9e852af',
-          fieldMap: {
-            fuel_type: 'Fuel_Type_Code',
-          },
-          config: {
-            show_label: 'false',
-          },
-          visualVariables: [
-            {
-              type: 'size',
-              valueExpression: '$view.scale',
-              stops: [
-                { value: scale / 2, size: 20 },
-                { value: scale * 2, size: 15 },
-                { value: scale * 4, size: 10 },
-                { value: scale * 8, size: 5 },
-                { value: scale * 16, size: 2 },
-                { value: scale * 32, size: 1 },
-              ],
-            },
-          ],
-        }),
-        minScale: 0,
-        maxScale: 10000,
-      });
+      // Generate GraphicsLayer instance
+      const layer = generateGraphicsLayerWithPoints(csvData);
 
-      const layer2 = new FeatureLayer({
-        url:
-          'https://services1.arcgis.com/4yjifSiIG17X0gW4/arcgis/rest/services/Alternative_Fuel_Station_March2018/FeatureServer',
-        outFields: ['*'],
-        renderer: new DictionaryRenderer({
-          url:
-            'https://jsapi.maps.arcgis.com/sharing/rest/content/items/30cfbf36efd64ccf92136201d9e852af',
-          fieldMap: {
-            fuel_type: 'Fuel_Type_Code',
-            connector_types: 'EV_Connector_Types',
-            network: 'EV_Network',
-            name: 'Station_Name',
-          },
-          config: {
-            show_label: 'true',
-          },
-        }),
-        minScale: 10000,
-        maxScale: 0,
-      });
+      // Log long, lat and country code to console
+      logLocationData(csvData);
 
-      const layer = pointsGenerator(csvData);
-      console.log('graphicsLayer: ', layer);
+      // Generate polyline for Austrian border
+      const austrianBorderLine = createBorder();
+      console.log('austrian border: ', austrianBorderLine);
 
-      logLocationData(layer);
+      layer.add(austrianBorderLine);
 
+      // Add the GraphicsLayer instance to the map
       map.addMany([layer]);
     }
   }, [csvData]);
